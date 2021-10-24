fab760f9d36b3f6b2cc2b8aa70ea648a512405fa
PassingG <49996889+PassingG@users.noreply.github.com>
2021-10-25 01:26:11 +0900

    Make Ui

--- Editor/BehaviourTreeEditor.cs
+++ Editor/BehaviourTreeEditor.cs
@@ -6,8 +6,8 @@ using UnityEditor.UIElements;
 
 public class BehaviourTreeEditor : EditorWindow
 {
-    [MenuItem("Window/UI Toolkit/BehaviourTreeEditor")]
-    public static void ShowExample()
+    [MenuItem("Zombist/BehaviourTreeEditor")]
+    public static void OpenWindow()
     {
         BehaviourTreeEditor wnd = GetWindow<BehaviourTreeEditor>();
         wnd.titleContent = new GUIContent("BehaviourTreeEditor");
@@ -18,20 +18,13 @@ public class BehaviourTreeEditor : EditorWindow
         // Each editor window contains a root VisualElement object
         VisualElement root = rootVisualElement;
 
-        // VisualElements objects can contain other VisualElement following a tree hierarchy.
-        VisualElement label = new Label("Hello World! From C#");
-        root.Add(label);
-
         // Import UXML
         var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/ZAI/Editor/BehaviourTreeEditor.uxml");
-        VisualElement labelFromUXML = visualTree.Instantiate();
-        root.Add(labelFromUXML);
+        visualTree.CloneTree(root);
 
         // A stylesheet can be added to a VisualElement.
         // The style will be applied to the VisualElement and all of its children.
         var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/ZAI/Editor/BehaviourTreeEditor.uss");
-        VisualElement labelWithStyle = new Label("Hello World! With Style");
-        labelWithStyle.styleSheets.Add(styleSheet);
-        root.Add(labelWithStyle);
+        root.styleSheets.Add(styleSheet);
     }
 }
